0457015be4bdfb823bad518d6219418462061cb3
jakobfp <s0543983@htw-berlin.de>
2018-12-01 15:01:54 +0100

    create new temp dir for each upload

--- converter.py
+++ converter.py
@@ -1,13 +1,26 @@
-from flask import send_from_directory
+import os
 import sys
+import time
+import hashlib
+
+from flask import send_from_directory
+
 sys.path.append('../pandocwrapper')
 
 import pandocwrapper
-import os
 
 LATEX_FILES = './cis/latex'
 UPLOAD_FOLDER = os.path.join(LATEX_FILES, "uploads")
 ALLOWED_UPLOAD_EXT = {'tex', 'docx'}
+
+
+def split_and_get_last_element(split_chr: str, string_to_split: str):
+    return string_to_split.split(split_chr)[len(string_to_split.split(split_chr)) - 1]
+
+
+def mkdir(directory: str):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
 
 def allowed_file(filename):
@@ -17,16 +30,22 @@ def allowed_file(filename):
 
 def upload(file):
     if allowed_file(file.filename):
-        file.save(os.path.join(UPLOAD_FOLDER, file.filename))
-        return {"success": True}
+        new_dir = hashlib.sha3_224(str(time.time()).encode('utf-8')).hexdigest()
+        mkdir(os.path.join(UPLOAD_FOLDER, new_dir))
+        save_path = os.path.join(UPLOAD_FOLDER, new_dir, file.filename)
+        file.save(save_path)
+        rel_save_path = split_and_get_last_element("/latex/", save_path)
+        return {"success": True, "file_path": rel_save_path}
     else:
         return {"success": False, "error": "file not supported, please upload either .tex or .docx"}
 
 
 def convert(file: str, design: str, bib_file: str = None):
+    print(file)
     path_to_files = LATEX_FILES
-    file = os.path.join('uploads', file.split('\\')[len(file.split('\\'))-1])
-    if os.path.exists(os.path.join(path_to_files, file)):
+    # filename = split_and_get_last_element("\\", file)
+    # file = os.path.join('uploads', filename)
+    if os.path.exists(os.path.join(LATEX_FILES, file)):
         template_string = design + ".tex"
         tex_converter = pandocwrapper.LatexConverter(file_in=file,
                                                      template=template_string,
@@ -34,8 +53,11 @@ def convert(file: str, design: str, bib_file: str = None):
                                                      bib=bib_file)
         tex_converter.construct_command()
         result = tex_converter.convert()
+
+        output_filename = split_and_get_last_element("/", tex_converter.file_out)
+
         if result is None:
-            return {"success": True, "file_path": tex_converter.file_out}
+            return {"success": True, "file_path": tex_converter.file_out, "file_name": output_filename}
         else:
             return {"error": result}
 
