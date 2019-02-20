add7b43a413e7e64334a8c17a9a46c085809a2e9
jakobfp <s0543983@htw-berlin.de>
2019-02-20 19:27:15 +0100

    bug fixes

--- markdown_presentation.py
+++ markdown_presentation.py
@@ -15,14 +15,17 @@ def write_to_file(markdown, path):
     return file_name
 
 
-def format_markdown(slides, title_slide):
+def format_markdown(slides, title_slides):
+    markdown_string = ""
 
-    markdown_string = "---\n"
-    markdown_string += "title: {}\n".format(title_slide['title'])
-    markdown_string += "subtitle: {}\n".format(title_slide['subtitle'])
-    markdown_string += "author: {}\n".format(title_slide['author'])
-    markdown_string += "date: {}\n".format(title_slide['date'])
-    markdown_string += "---\n"
+    if len(title_slides) > 0:
+        title_slide = title_slides[0]
+        markdown_string = "---\n"
+        markdown_string += "title: {}\n".format(title_slide['title'])
+        markdown_string += "subtitle: {}\n".format(title_slide['subtitle'])
+        markdown_string += "author: {}\n".format(title_slide['author'])
+        markdown_string += "date: {}\n".format(title_slide['date'])
+        markdown_string += "---\n"
 
     for slide in slides:
         markdown_string += "\n## {}\n".format(slide['title'])
@@ -31,12 +34,18 @@ def format_markdown(slides, title_slide):
             content = "\n\\colA{{6cm}}\n\n{}\n\n\\colB{{6cm}}\n\n{}\n\n\\colEnd\n\n".format(slide['col1'], slide['col2'])
         markdown_string += content
 
-    print(markdown_string)
-
     return markdown_string
 
 
+def convert(file_path):
+    return True
+
+
 def create(all_slides):
-    markdown = format_markdown(all_slides['slides'], all_slides['titleSlides'][0])
-    file_name = write_to_file(markdown, os.path.join(ROOT))
+
+    if len(all_slides['titleSlides']) is 0 and len(all_slides['slides']) is 0:
+        return {"success": False, "file_path": "", "error": "no slides"}
+
+    markdown = format_markdown(all_slides['slides'], all_slides['titleSlides'])
+    file_name = write_to_file(markdown, os.path.join(ROOT, UPLOAD_FOLDER))
     return {"success": True, "file_path": file_name, "error": ""}
